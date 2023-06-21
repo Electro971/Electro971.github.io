9166c6a455da6850c005d90802c4ce23469d0885
Electro971 <127629123+Electro971@users.noreply.github.com>
2023-06-20 22:42:48 -0400

    Change to Goblin instead of placeholder letters

--- script.js
+++ script.js
@@ -2,44 +2,82 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function load()
+function firstLoad()
 {
 	kills=0;
 	gold=0;
 	mana=0;
 	zone = 1;
-	stage=0;
+	stage=1;
 	counter = 0;
 	level=1;
 	
-	setMonLevel(1);
-	setHP();
-	updateZone();
+	if (JSON.parse(localStorage.getItem("gameSave")) !== null)
+		load();
+	else
+	{
+		newMonster();
+		setHP(10);
+	}
+	
+	globalUpdate();
+}
+
+function globalUpdate() {
+	setHP(10);
+	updateStage();
 	updateGold();
 	updateKills();
-	newMonster();
+	updateMonster();
 }
 
+//saving
 function save()
 {
-	localStorage.setItem("monster", document.getElementById("monster"));
-	localStorage.setItem("kills", kills);
-	localStorage.setItem("gold", gold);
-	localStorage.setItem("hp", hp);
+	gameSave = {
+		gold: gold,
+		hp: hp,
+		zone: zone,
+		level: level,
+		kills: kills,
+		
+	};
+	localStorage.setItem("gameSave", JSON.stringify(gameSave));
 }
 
-function newMonster()
-{
-	setMonLevel(1);
-	setHP();
-	document.getElementById("monster").src=getRandomInt(3) + ".png";
-	document.getElementById("monster-label").innerHTML = "Current Monster: "+document.getElementById("monster").src;
-	document.getElementById("hp-label").innerHTML = "HP Left: "+hp;
+//autosave
+setInterval (function() {
+	 save();
+}, 300000); //3 minutes
+
+function load() {
+	savedGame = JSON.parse(localStorage.getItem("gameSave"));
+	
+	if (typeof savedGame.gold !== "undefined")
+		gold = savedGame.gold;
+	else
+		gold = -1;
+	if (typeof savedGame.hp !== "undefined")
+		hp = savedGame.hp;
+	else hp = setHP();
+	if (typeof savedGame.zone !== "undefined")
+		zone = savedGame.zone;
+	else
+		zone = 1;
+	if (typeof savedGame.level !== "undefined")
+		level = savedGame.level;
+	else
+		level = 0;
+	if (typeof savedGame.kills !== "undefined")
+		kills = savedGame.kills;
+	else
+		kills = -1;
+	globalUpdate();
 }
 
-function setHP()
+function setHP(x)
 {
-	hp=10;
+	hp=x;
 }
 
 function setMonLevel(x)
@@ -51,19 +89,13 @@ function setMonLevel(x)
 function attack()
 {
 	hp-=1;
-	document.getElementById("hp-label").innerHTML = "HP Left: "+hp;
+	updateMonster()
 	if (hp <= 0)
 		monsterKill();
 }
 
-function updateZone()
+function updateStage()
 {
-	stage+=1;
-	if (stage >= 6)
-	{
-		zone+=1
-		stage=1
-	}
 	if (zone < 4)
 	{
 		document.getElementById("zone-name").innerHTML = "The Fieldlands"
@@ -71,26 +103,64 @@ function updateZone()
 	document.getElementById("zone-numbers").innerHTML = zone + "-" + stage;
 }
 
+function clearStage() 
+{
+	stage+=1;
+	if (stage >= 6)
+	{
+		zone+=1
+		stage=1
+	}
+	updateStage();
+}
+
+function updateMonster()
+{
+	if (document.getElementById("monster").src.includes("goblin"))
+		document.getElementById("monster-name").innerHTML = "Goblin";
+	else 
+		document.getElementById("monster-name").innerHTML = "null";
+	document.getElementById("hp-num").innerHTML = hp;
+}
+
 function monsterKill()
 {
-	updateKills();
-	updateGold();
+	addKill();
+	addGold();
 	counter+=1;
 	if (counter >= 4)
 	{
-		updateZone();
+		clearStage();
 		counter = 0;
 	}
 	newMonster();
 }
 
+function newMonster()
+{
+	setMonLevel(1);
+	setHP(10);
+	document.getElementById("monster").src = "goblin.jpg";
+	updateMonster();
+}
+
 function updateKills()
+{
+	document.getElementById("kills-label").innerHTML = "Total Kills: " + kills;
+}
+
+function addKill()
 {
 	kills+=1;
 	document.getElementById("kills-label").innerHTML = "Total Kills: " + kills;
 }
 
 function updateGold()
+{
+	document.getElementById("gold-label").innerHTML = "Gold: " + gold;
+}
+
+function addGold()
 {
 	gold += 1;
 	document.getElementById("gold-label").innerHTML = "Gold: " + gold;
