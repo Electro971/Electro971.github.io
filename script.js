d8c689d001f199b53e6af04c7baef55d16b89930
Hal Shuler <127629123+Electro971@users.noreply.github.com>
2023-11-01 11:39:21 -0400

    Messages and started fishing
    Finished the message() function so that it works (beautifully I might add) and created the beginning of a fish() function. All it does as of now is disable the button and wait for 0-10 seconds before alerting.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var messages = 0;
+var fishingSpeed = 1;
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -13,53 +16,91 @@ function save()
 		
 	};
 	
-	heroFile = {
-		
-	}
-	
-	
 	localStorage.setItem("gameSave", JSON.stringify(gameSave));
-	localStorage.setItem("heroFile", JSON.stringify(heroFile));
 }
 
 function load() 
 {
 	savedGame = JSON.parse(localStorage.getItem("gameSave"));
-	heroFile = JSON.parse(localStorage.getItem("heroFile"));
 	
 	//Info
 	if (savedGame != null)
 	{
-	
-	}
-	else
-	{
-	
-	}
-	
-	if (heroFile != null)
-	{
 		
 	}
 	else
 	{
-		
+	
 	}
 	globalUpdate();
 }
 
-function message(msg)
+function fish(biome)
 {
-var div = document.createElement("message1");
-div.style.width = "100%";
-div.style.height = "auto";
-div.style.background = "gray";
-div.style.color = "white";
-div.innerHTML = msg;
-onclick = this.remove
-
-document.getElementById("log-box").appendChild(div);	
+	if(biome == "default")
+	{
+		document.getElementById("fish-name").innerHTML = "???"
+		document.getElementById("fish-button").disabled = true;
+		setTimeout(function(){
+			alert("you got one");
+			document.getElementById("fish-name").innerHTML = "Nothing"
+			document.getElementById("fish-button").disabled = false;
+		}, (getRandomInt(10)*1000)/fishingSpeed);
+	}
 }
+
+//Sends a message to the log by taking the text, the number of buttons, then the text of the button(s).
+function message(msg, buttons, button1, button2)
+{
+	var div = document.createElement("message"+messages);
+	div.style.width = "100%";
+	div.style.height = "auto";
+	div.style.background = "gray";
+	div.style.color = "white";
+	div.style.border = "solid black 2px";
+	div.style.padding = "2px 2px 2px 2px";
+	div.innerHTML = msg;
+
+	messages++;
+	document.getElementById("log-box").appendChild(div);
+	
+	if (buttons == 1 || buttons ==2)
+	{	
+		var buttonNumOne = document.createElement("BUTTON");
+		buttonNumOne.innerText = button1;
+		buttonNumOne.id = "message"+messages+"Button"+1;
+		if (buttons == 2)
+		{
+			var buttonNumTwo = document.createElement("BUTTON");
+			buttonNumTwo.innerText = button2;
+			buttonNumTwo.id = "message"+messages+"Button"+2;
+		}
+		document.getElementById('log-box').appendChild(buttonNumOne);
+		document.getElementById('log-box').appendChild(buttonNumTwo);
+	}
+}
+
+var flag0 = false;
+
+ function plot()
+ {
+	if (!flag0)
+	{
+		message("You wake up on the beach with no idea how you got here.",0);
+		flag0 = true;
+		setTimeout(function(){
+			message("Nothing to do but move on you suppose.",0);
+		}, 1000);
+		setTimeout(function(){
+			message("Seems like all you have is an old fishing rod. How odd.",0); 
+		}, 2000);
+	}
+ }
+
+// Checks plot/messages every second
+setInterval (function() {
+	 plot();
+}, 100);
 
 //autosave
 /* setInterval (function() {
